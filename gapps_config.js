3c2142e5948804f4bed5d14e6c2c0fbd07053acd
Vernon <cullinan30a@gmail.com>
2025-08-09 19:09:23 +0800

    docs: update app script config and index version

---
+++ gapps_config.js
@@ -0,0 +1,13 @@
+export const APP_SCRIPT_ID = '1lcO17egk_Jd7o9Al94MtO8UAUzzO49lYUdAJzAdRUCxN7dQ9OGgOm1nj';
+export const DEPLOYMENT_ID = 'AKfycbzLupXuHa5MjL5GyYYQMCukzfPFJz2q2pCDcI45H0voyA2IV1X3Y6g6oDAVJIA0PWFQ';
+export const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzLupXuHa5MjL5GyYYQMCukzfPFJz2q2pCDcI45H0voyA2IV1X3Y6g6oDAVJIA0PWFQ/exec';
+export const WEBHOOK_PASSCODE = 'nucsup-vunset-4hyhnl';
+export const CHATGPT_FOLDER_ID = '1dVrvGaIwFAaM4qdWxk_DLeKnYbIJjxxd';
+
+export default {
+  APP_SCRIPT_ID,
+  DEPLOYMENT_ID,
+  WEB_APP_URL,
+  WEBHOOK_PASSCODE,
+  CHATGPT_FOLDER_ID
+};
